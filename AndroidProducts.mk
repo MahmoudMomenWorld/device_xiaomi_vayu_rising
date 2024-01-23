e7cf47d0219cb97a817d950d2b43107cf72bd1a2
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-01-23 20:41:53 +0000

    initial Bringup

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2021 aospOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_vayu.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_vayu-eng \
+    lineage_vayu-userdebug \
+    lineage_vayu-user
