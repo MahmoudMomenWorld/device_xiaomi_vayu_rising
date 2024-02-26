ed5f6669d01c9f3c47aebf5493d558d1584691d6
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-02-26 03:03:25 +0200

    Including Sepolicy for MIUI Camera Thanks to dogpoopy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -158,7 +158,8 @@ TARGET_USES_MKE2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr/sm8150/SEPolicy.mk
+include vendor/xiaomi/vayu-miuicamera/products/board.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
