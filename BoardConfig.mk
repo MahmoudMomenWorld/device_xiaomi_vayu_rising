6c6d9501c7e1e4275e5b1ed8aa4c5390429b1c87
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-06-08 18:32:42 +0300

    Temporary disabling miui camera

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
@@ -159,7 +159,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-include vendor/xiaomi/vayu-miuicamera/products/board.mk
+#include vendor/xiaomi/vayu-miuicamera/products/board.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
