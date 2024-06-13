131f2ed916e77b39d4ba1e86ef605ab91bbbcba1
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-06-13 22:35:28 +0300

    Correction of  inheriting risingos stuff

--- rising_vayu.mk
+++ rising_vayu.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common AOSP configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rising/config/rising.mk)
 
 
 # Device identifier
